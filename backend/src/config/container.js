1cc038f270aa08542618f5aacde095fdf8722114
BiggillyBoyMan <lucaslow57@gmail.com>
2026-03-11 12:08:14 +0800

    init employees api endpoint and done get/employees endpoint

--- backend/src/config/container.js
+++ backend/src/config/container.js
@@ -4,9 +4,9 @@ const pool = require('./database')
 const Mediator = require('../mediator/Mediator')
 
 const CafeController = require('../cafes/CafeController')
+const CafeRepository = require('../cafes/CafeRepository')
 
 // Get/cafes - location
-const CafeRepository = require('../cafes/CafeRepository')
 const GetCafesHandler = require('../cafes/handlers/GetCafesHandler')
 const GetCafesQuery = require('../cafes/queries/GetCafesQuery')
 
@@ -18,6 +18,16 @@ const CreateCafeHandler = require('../cafes/handlers/CreateCafeHandler')
 const UpdateCafeCommand = require('../cafes/commands/UpdateCafeCommand')
 const UpdateCafeHandler = require('../cafes/handlers/UpdateCafeHandler')
 
+//Delete /Cafes
+const DeleteCafeCommand = require('../cafes/commands/DeleteCafeCommand')
+const DeleteCafeHandler = require('../cafes/handlers/DeleteCafeHandler')
+
+const EmployeeController = require('../employees/EmployeeController')
+const EmployeeRepository = require('../employees/EmployeeRepository')
+
+//Get Employees
+const GetEmployeesQuery = require('../employees/queries/GetEmployeesQuery')
+const GetEmployeesHandler = require('../employees/handlers/GetEmployeesHandler')
 
 const container = createContainer()
 
@@ -29,6 +39,10 @@ container.register({
   getCafesHandler: asClass(GetCafesHandler).singleton(),
   createCafeHandler: asClass(CreateCafeHandler).singleton(),
   updateCafeHandler: asClass(UpdateCafeHandler).singleton(),
+  deleteCafeHandler: asClass(DeleteCafeHandler).singleton(),
+  employeeController: asClass(EmployeeController).singleton(),
+  employeeRepository: asClass(EmployeeRepository).singleton(),
+  getEmployeesHandler: asClass(GetEmployeesHandler).singleton(),
 })
 
 const mediator = container.resolve('mediator')
@@ -38,5 +52,9 @@ mediator.register(GetCafesQuery.name, container.resolve('getCafesHandler'))
 mediator.register(CreateCafeCommand.name, container.resolve('createCafeHandler'))
 //update cafe API
 mediator.register(UpdateCafeCommand.name, container.resolve('updateCafeHandler'))
+//delete cafe API
+mediator.register(DeleteCafeCommand.name, container.resolve('deleteCafeHandler'))
+//get employees API
+mediator.register(GetEmployeesQuery.name, container.resolve('getEmployeesHandler'))
 
 module.exports = container
