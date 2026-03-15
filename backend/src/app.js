296c7483829af168e3b676aac830cf220a845afb
BiggillyBoyMan <lucaslow57@gmail.com>
2026-03-15 13:14:58 +0800

    initialising docker for prod + setting up .env variables

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,9 @@ const errorHandler = require('./middleware/errorHandler')
 
 const app = express();
 
-app.use(cors())
+app.use(cors({
+  origin: process.env.ALLOWED_ORIGIN || 'http://localhost:5173'
+}))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
